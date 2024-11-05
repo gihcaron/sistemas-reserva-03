d668cc4eaa1e47f907e2cf9755d01e3ec11a71be
Giovanna Caron <158209996+gihcaron@users.noreply.github.com>
2024-11-05 09:14:19 -0300

    feat: criação da tabelas Hospedes e Quartos

--- reserva.sql
+++ reserva.sql
@@ -3,5 +3,12 @@ CREATE DATABASE revervas_hotel;
 CREATE TABLE Hospedes(
     id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
+    idade INT NOT NULL,
+    email VARCHAR(150) NOT NULL
+);
+
+CREATE TABLE Quartos(
+    id SERIAL PRIMARY KEY,
+    numero_quarto INT UNIQUE NOT NULL,
     
 )
