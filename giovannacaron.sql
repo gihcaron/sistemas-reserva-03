bcd7a14a63c19bf87e4f201a837dbc9a79aca1e3
Giovanna <giovannacaron.tds1.senai@gmail.com>
2024-11-05 12:09:42 -0300

    feat: finalização da criação de tabelas

--- giovannacaron.sql
+++ giovannacaron.sql
@@ -2,19 +2,29 @@ CREATE DATABASE revervas_hotel;
 
 --  armazena as informações dos hóspedes
 CREATE TABLE hospedes(
-    id SERIAL PRIMARY KEY,
+    id_hospede SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     idade INT NOT NULL,
+    telefone VARCHAR (23),
     email VARCHAR(150) NOT NULL
 );
 
-
+-- Quartos contém os dados dos quartos disponíveis no hotel
 CREATE TABLE quartos(
-    id SERIAL PRIMARY KEY,
+    id_quarto SERIAL PRIMARY KEY,
     numero_quarto INT UNIQUE NOT NULL,
-    
-)
+    quarto_disponivel BOOLEAN NOT NULL DEFAULT TRUE,
+    valor_diaria DECIMAL (12,2) NOT NULL
+);
 
+--  tabela Reservas registra as reservas realizadas pelos hóspedes
 CREATE TABLE reservas(
-
-)
+    id_reserva SERIAL PRIMARY KEY,
+        id_hospede INT NOT NULL,
+    id_quarto INT NOT NULL,
+    data_entrada DATE NOT NULL DEFAULT CURRENT_DATE,
+    data_saida DATE NOT NULL DEFAULT CURRENT_DATE,
+    hora_reserva TIME NOT NULL,
+    CONSTRAINT fk_hospede FOREIGN KEY (id_hospede) REFERENCES hospedes(id_hospede),
+    CONSTRAINT fk_quarto FOREIGN KEY (id_quarto) REFERENCES quartos(id_quarto)
+);
