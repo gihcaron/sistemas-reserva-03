17ae5ae76a9579c804b1bef21e4f1f54abdfc77d
Giovanna <giovannacaron.tds1.senai@gmail.com>
2024-11-05 14:07:04 -0300

    Feat: inserção de hospedes

--- giovannacaron.sql
+++ giovannacaron.sql
@@ -28,3 +28,20 @@ CREATE TABLE reservas(
     CONSTRAINT fk_hospede FOREIGN KEY (id_hospede) REFERENCES hospedes(id_hospede),
     CONSTRAINT fk_quarto FOREIGN KEY (id_quarto) REFERENCES quartos(id_quarto)
 );
+
+-- Inserção de dados
+
+INSERT INTO hospedes (nome, idade, email, telefone) 
+VALUES 
+('Giovanna Caron', 20, 'giovannacaron@gmail.com', '(11) 12345678'),
+('Taylor Swift', 27, 'taylorswift@gmail.com', '(11) 33333333'),
+('Harry Styles', 30, 'harrystyles@gmail.com', '(11) 44444444'),
+('Zayn Malik', 30, 'zaynmalik@gmail.com', '(11) 55555555'),
+('Rihanna', 40, 'rihana@gmail.com', '(11) 12345644'),
+('Luan Santana', 36, 'santanaluan10@gmail.com', '(34) 12345555'),
+('Rafaela Ferreira', 18, 'rafaela@gmail.com', '(43) 09845678'),
+('Rachel Loh', 40, 'lohrachel@outook.com', '(21) 12345678'),
+('Katy Perry', 20, 'perrykty@gmail.com', '(81) 12367878'),
+('Ana Caetano', 20, 'giovannacaron@gmail.com', '(11) 12345678'),
+('Thiago Ferreira', 20, 'ferreirathiago@outlook.com', '(28) 02845678'),
+('Bruno Mars', 28, 'marsbruno@gmail.com', '(11) 88345667');
