e9fc93e1e97c329e15ac220bdc5a7ed0a3d011c6
Giovanna <giovannacaron.tds1.senai@gmail.com>
2024-11-05 11:53:36 -0300

    Docs: arquivo renomeado

--- giovannacaron.sql
+++ giovannacaron.sql
@@ -1,14 +1,20 @@
 CREATE DATABASE revervas_hotel;
 
-CREATE TABLE Hospedes(
+--  armazena as informações dos hóspedes
+CREATE TABLE hospedes(
     id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     idade INT NOT NULL,
     email VARCHAR(150) NOT NULL
 );
 
-CREATE TABLE Quartos(
+
+CREATE TABLE quartos(
     id SERIAL PRIMARY KEY,
     numero_quarto INT UNIQUE NOT NULL,
     
+)
+
+CREATE TABLE reservas(
+
 )
